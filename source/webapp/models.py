4a22da8f97c5673e3e16e1e2819e8187ebbddd9e
Айсулу <aisuluueco2009@yandex.ru>
2019-11-15 15:30:11 +0600

    Добавила в модель Product поле 'в наличии'  и добавила в ORDER_STATUS_CHOICES 'Отменен'.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
                                 verbose_name='Категория')
     photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Фото')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    in_order = models.BooleanField(verbose_name='В наличии', default=True)
 
     def __str__(self):
         return self.name
@@ -29,7 +30,8 @@ ORDER_STATUS_CHOICES = (
    ('new', 'Новый'),
    ('payed', 'Оплачен'),
    ('processing', 'Обработка'),
-   ('delivered', 'Доставлен')
+   ('delivered', 'Доставлен'),
+   ('canceled', 'Отменён')
 )
 
 
